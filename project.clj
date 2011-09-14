111df48a814a31000e527e8df37ae86c40dc45aa
scott <juveduke@gmail.com>
2011-09-14 18:22:24 -0400

    Upgraded to the latest for compojure, hiccup, ring. Made some of the statements more concise.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject ml "0.1"
-    :dependencies [[ring "0.3.0"]
+    :dependencies [[ring "0.3.11"]
                    [incanter "1.2.3"]
-                   [compojure "0.5.0"]
-                   [hiccup "0.3.0"]]
+                   [compojure "0.6.5"]
+                   [hiccup "0.3.6"]]
     :main ml.core)
--- src/ml/math.clj
+++ src/ml/math.clj
@@ -10,10 +10,11 @@
 (defn rands-unq
   "Returns a random selection of n unique numbers in the range 0 to max.
   Any numbers in the exclude seq won't be selected."
-  [max n exclude]
-  (if (= (count exclude) n)
-    exclude
-    (let [x (rand-int max)]
-      (if (some #(= x %) exclude)
-        (rands-unq max n exclude)
-        (rands-unq max n (concat exclude [x]))))))
+  ([max n] (rands-unq max n []))
+  ([max n exclude]
+    (if (= (count exclude) n)
+      exclude
+      (let [x (rand-int max)]
+        (if (some #{x} exclude)
+          (rands-unq max n exclude)
+          (rands-unq max n (concat exclude [x])))))))
